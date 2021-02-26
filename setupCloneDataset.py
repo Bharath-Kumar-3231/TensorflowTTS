ac58e1fdc9ec941a2dcf071158bfeb7817785fbf
aragorntheking <57813256+aragorntheking@users.noreply.github.com>
2021-02-26 11:43:56 +0530

    Update setupCloneDataset.py

--- setupCloneDataset.py
+++ setupCloneDataset.py
@@ -18,29 +18,35 @@ def query_task(task_id, dynamodb=None):
 
 if __name__ == '__main__':
   # Instantiate the parser
-    parser = argparse.ArgumentParser(description='Pass task id to save parameters')
+    parser = argparse.ArgumentParser(
+        description='Pass task id to save parameters')
     # Optional argument
     parser.add_argument('--task_id', type=str, help='Task id to train on')
-    parser.add_argument('--libri_path', type=str, help='Folder to dump speaker folders')
-    parser.add_argument('--dataset_path', type=str, help='Path to core speaker folders')
-    
+    parser.add_argument('--libri_path', type=str,
+                        help='Folder to dump speaker folders')
+    parser.add_argument('--dataset_path', type=str,
+                        help='Path to core speaker folders')
+
     args = parser.parse_args()
     print("Argument values:")
     print(args.task_id)
+    print(args.libri_path)
+    print(args.dataset_path)
     task = query_task(args.task_id)
-    print(task)
     s3 = boto3.resource('s3')
     os.mkdir(args.libri_path)
-    
+
     for i in task['speakerIds']:
-        tarPath = args.libri_path+"/"+str(i)+".tar"
-        s3.Bucket('murf-models-dev').download_file("dataset/speakers/"+str(i)+".tar", tarPath)
+        tarPath = args.libri_path + "/" + str(i) + ".tar"
+        s3Key = "dataset/speakers/" + str(i) + ".tar"
+        print("Downloading " + s3Key)
+        s3.Bucket('murf-models-dev').download_file(s3Key, tarPath)
         tar = tarfile.open(tarPath)
         tar.extractall(path=args.libri_path)
         tar.close()
         os.remove(tarPath)
         print("Extracted"+str(i))
-    
+
     for i in os.listdir(args.dataset_path):
         if i.isnumeric():
             speakerDestFolderPath = os.path.join(args.libri_path, i)
@@ -49,7 +55,11 @@ if __name__ == '__main__':
             for fileName in os.listdir(speakerSrcFolderPath):
                 if ".wav" in fileName:
                     wavsrc = os.path.join(speakerSrcFolderPath, fileName)
-                    txtsrc = os.path.join(speakerSrcFolderPath, fileName.replace(".wav",".txt"))
-                    copyfile(wavsrc, os.path.join(speakerDestFolderPath, fileName))        
-                    copyfile(txtsrc, os.path.join(speakerDestFolderPath, fileName.replace(".wav",".txt")))
+                    txtsrc = os.path.join(
+                        speakerSrcFolderPath, fileName.replace(".wav", ".txt"))
+                    copyfile(wavsrc, os.path.join(
+                        speakerDestFolderPath, fileName))
+                    copyfile(txtsrc, os.path.join(
+                        speakerDestFolderPath, fileName.replace(".wav", ".txt")))
                     break
+
