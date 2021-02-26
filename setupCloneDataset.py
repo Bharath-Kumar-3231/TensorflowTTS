5398cf42f9ae0e07143fac3be89f01469d184d61
aragorntheking <57813256+aragorntheking@users.noreply.github.com>
2021-02-26 10:40:02 +0530

    Update setupCloneDataset.py

--- setupCloneDataset.py
+++ setupCloneDataset.py
@@ -1,6 +1,9 @@
 import boto3
 import argparse
 from boto3.dynamodb.conditions import Key
+import tarfile
+import os
+from shutil import copyfile
 
 def query_task(task_id, dynamodb=None):
     if not dynamodb:
@@ -18,12 +21,34 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pass task id to save parameters')
     # Optional argument
     parser.add_argument('--task_id', type=str, help='Task id to train on')
-    parser.add_argument('--path', type=str, help='Properties file path to dump task details')
+    parser.add_argument('--libri_path', type=str, help='Folder to dump speaker folders')
+    parser.add_argument('--dataset_path', type=str, help='Path to core speaker folders')
+    
     args = parser.parse_args()
     print("Argument values:")
     print(args.task_id)
     task = query_task(args.task_id)
     print(task)
-    f = open(args.path, "a")
-    f.write(','.join([str(i) for i in task['speakerIds']]))
-    f.close()
+    s3 = boto3.resource('s3')
+    
+    for i in task['speakerIds']:
+        tarPath = args.libri_path+"/"+str(i)+".tar"
+        s3.Bucket('murf-models-dev').download_file("dataset/speakers/"+str(i)+".tar", tarPath)
+        tar = tarfile.open(tarPath)
+        tar.extractall(path=args.libri_path)
+        tar.close()
+        os.remove(tarPath)
+        print("Extracted"+str(i))
+    
+    for i in os.listdir(args.dataset_path):
+        if i.isnumeric():
+            speakerDestFolderPath = os.path.join(args.libri_path, i)
+            os.mkdir(speakerDestFolderPath)
+            speakerSrcFolderPath = os.path.join(args.dataset_path, i)
+            for fileName in os.listdir(speakerSrcFolderPath):
+                if ".wav" in fileName:
+                    wavsrc = os.path.join(speakerSrcFolderPath, fileName)
+                    txtsrc = os.path.join(speakerSrcFolderPath, fileName.replace(".wav",".txt"))
+                    copyfile(wavsrc, os.path.join(speakerDestFolderPath, fileName))        
+                    copyfile(txtsrc, os.path.join(speakerDestFolderPath, fileName.replace(".wav",".txt")))
+                    break
