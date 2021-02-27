5e4c3a20509f33b4f8426f247a0cf108f11874d8
aragorntheking <57813256+aragorntheking@users.noreply.github.com>
2021-02-27 08:38:08 +0530

    Update train_vocoder_ft.sh

--- train_vocoder_ft.sh
+++ train_vocoder_ft.sh
@@ -93,7 +93,7 @@ fi
 
 latestModelPath=$(ls -t $CKPT_DIR/generator-*h5 | head -1)
 modelExists=false
-if [[ $latestmodelPath == *"h5" ]]; 
+if [[ $latestModelPath == *"h5" ]]; 
 then
   modelExists=true
 fi
